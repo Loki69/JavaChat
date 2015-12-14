b104f6eb7ae6c2bfec73a825d4818a7633312f05
Pavel Garanin <mur069@yandex.ru>
2015-12-14 14:18:55 +0200

    add set port on run

--- server/src/main/java/com/mycompany/server/Main.java
+++ server/src/main/java/com/mycompany/server/Main.java
@@ -8,14 +8,14 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-//        if(args.length > 0){
+        if(args.length > 0){
         try {
-            new Thread(Server.init(1054)).start();
+            new Thread(Server.init(Integer.parseInt(args[0]))).start();
         } catch (ClassNotFoundException | SQLException | IOException ex) {
             System.err.println(ex.getMessage());
         }
-//        }else{
-//            System.out.println("port init wos not set");
-//        }
+        }else{
+            System.out.println("port init wos not set");
+        }
     }
 }
